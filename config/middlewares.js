d14e502cbc0d6cd001604180e090c17218550f1e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-17 13:27:11 -0300

    Deploy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,39 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  (module.exports = [
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "dl.airtable.com",
+              "res.cloudinary.com",
+            ],
+            "media-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "dl.airtable.com",
+              "res.cloudinary.com",
+            ],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+  ]),
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
