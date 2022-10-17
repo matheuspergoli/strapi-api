da544805439b82dbdb0b4df41de8d0ad395792d3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-17 13:28:09 -0300

    Deploy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,33 +1,31 @@
 module.exports = [
   "strapi::errors",
-  (module.exports = [
-    {
-      name: "strapi::security",
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            "connect-src": ["'self'", "https:"],
-            "img-src": [
-              "'self'",
-              "data:",
-              "blob:",
-              "dl.airtable.com",
-              "res.cloudinary.com",
-            ],
-            "media-src": [
-              "'self'",
-              "data:",
-              "blob:",
-              "dl.airtable.com",
-              "res.cloudinary.com",
-            ],
-            upgradeInsecureRequests: null,
-          },
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
         },
       },
     },
-  ]),
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
