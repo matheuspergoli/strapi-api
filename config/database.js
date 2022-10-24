8d27aeb470fca64c10e81352b73832104fa8644c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-23 22:06:18 -0300

    Update API

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'heffalump.db.elephantsql.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'vfvdnrgu'),
-      user: env('DATABASE_USERNAME', 'vfvdnrgu'),
-      password: env('DATABASE_PASSWORD', 'PUS3mdIMhirSjfgrGx_xpg2fDlCGpXHz'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
